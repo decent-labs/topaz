a6e3f03ea448d38075e3dc713f0eac59fe288f2a
parker mccurley <parker@decentcrypto.com>
2018-10-08 03:16:35 -0400

    complete 'store' functionality

--- store.go
+++ store.go
@@ -7,19 +7,23 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 
 	shell "github.com/ipfs/go-ipfs-api"
 	_ "github.com/lib/pq"
 )
 
-// Take the request body and store it in IPFS, then store the resulting hash in the `queue` table.
+// Take the request body and store it in IPFS, then store the resulting hash in the `objects` table.
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	sh := shell.NewShell("localhost:5001")
+	shConn := fmt.Sprintf("%s:%s", os.Getenv("IPFS_HOST"), os.Getenv("IPFS_PORT"))
+	sh := shell.NewShell(shConn)
 	br := bytes.NewReader(b)
 
 	hash, err := sh.Add(br)
@@ -27,12 +31,11 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	dbConn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s",
-		"localhost",
-		"5432",
-		"postgres",
-		"postgres",
-		"disable",
+	dbConn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable",
+		os.Getenv("PQ_HOST"),
+		os.Getenv("PQ_PORT"),
+		os.Getenv("PQ_USER"),
+		os.Getenv("PQ_NAME"),
 	)
 
 	db, err := sql.Open("postgres", dbConn)
@@ -41,9 +44,20 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	insert := fmt.Sprintf("INSERT INTO queue (hash) VALUES ('%s') RETURNING hash", hash)
+	out, err := exec.Command("uuidgen").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	_, err = db.Exec(insert)
+	id := strings.TrimSpace(fmt.Sprintf("%s", out))
+
+	stmt := fmt.Sprintf("insert into objects (id, hash, user_id) values ('%s', '%s', '%s')",
+		id,
+		hash,
+		os.Getenv("TOPAZ_USER"),
+	)
+
+	_, err = db.Exec(stmt)
 	if err != nil {
 		log.Fatal(err)
 	}
