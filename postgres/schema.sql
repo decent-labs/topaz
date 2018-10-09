36310bcf00572c59bd658ec2e687ad77b83ef0bc
parker mccurley <parker@decentcrypto.com>
2018-10-08 23:44:12 -0400

    refactor 'postgres' service

---
+++ postgres/schema.sql
@@ -0,0 +1,177 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.5 (Debian 10.5-1.pgdg90+1)
+-- Dumped by pg_dump version 10.5 (Debian 10.5-1.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE postgres IS 'default administrative connection database';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: flushes; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.flushes (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    user_id uuid,
+    hash character varying(255),
+    created_at timestamp without time zone DEFAULT now()
+);
+
+
+ALTER TABLE public.flushes OWNER TO postgres;
+
+--
+-- Name: objects; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.objects (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    user_id uuid,
+    flush_id uuid,
+    hash character varying(255),
+    created_at timestamp without time zone DEFAULT now()
+);
+
+
+ALTER TABLE public.objects OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    name character varying(255),
+    "interval" interval,
+    created_at timestamp without time zone DEFAULT now(),
+    flushed_at timestamp without time zone
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Data for Name: flushes; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.flushes (id, user_id, hash, created_at) FROM stdin;
+\.
+
+
+--
+-- Data for Name: objects; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.objects (id, user_id, flush_id, hash, created_at) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, "interval", created_at, flushed_at) FROM stdin;
+966d1035-73a1-4e57-9d1f-95920524689a	parker	00:00:10	2018-10-09 03:18:26.761399	\N
+\.
+
+
+--
+-- Name: flushes flushes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.flushes
+    ADD CONSTRAINT flushes_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: objects objects_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.objects
+    ADD CONSTRAINT objects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: flushes flushes_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.flushes
+    ADD CONSTRAINT flushes_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: objects objects_flush_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.objects
+    ADD CONSTRAINT objects_flush_id_fkey FOREIGN KEY (flush_id) REFERENCES public.flushes(id);
+
+
+--
+-- Name: objects objects_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.objects
+    ADD CONSTRAINT objects_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
