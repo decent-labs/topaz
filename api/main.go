3fb708afabd3a72bb0d99adfc1d6b89b1ebb2d1e
Adam Gall <adam@decentcrypto.com>
2018-11-01 13:37:37 +0100

    Add middleware function to perform JWT bearer token authentication

--- api/main.go
+++ api/main.go
@@ -9,8 +9,10 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/gorilla/context"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 
@@ -236,6 +238,35 @@ func createTokenHandler(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
 
 	log.Println("finished with /authenticate (create token) handler")
+}
+
+func Authenticate(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		authorizationHeader := req.Header.Get("authorization")
+		if authorizationHeader != "" {
+			bearerToken := strings.Split(authorizationHeader, " ")
+			if len(bearerToken) == 2 {
+				token, error := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, fmt.Errorf("There was an error")
+					}
+					return []byte(os.Getenv("API_JWT_KEY")), nil
+				})
+				if error != nil {
+					json.NewEncoder(w).Encode(Exception{Message: error.Error()})
+					return
+				}
+				if token.Valid {
+					context.Set(req, "decoded", token.Claims)
+					next(w, req)
+				} else {
+					json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+				}
+			}
+		} else {
+			json.NewEncoder(w).Encode(Exception{Message: "An authorization header is required"})
+		}
+	})
 }
 
 func main() {
