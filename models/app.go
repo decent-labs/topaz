ee13ac131aabdff3bc3c7d2a87abfb6e05a61ca9
Adam Gall <adam@decentcrypto.com>
2018-11-08 16:46:29 -0500

    Add simple (cause of gorm) functions for GetApp and CreateApp to app model

--- models/app.go
+++ models/app.go
@@ -17,3 +17,17 @@ type App struct {
 	Objects     []Object   `json:"objects"`
 	EthAddress  string     `json:"ethAddress"`
 }
+
+func (a *App) CreateApp(db *gorm.DB) error {
+	if err := db.Create(&a).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *App) GetApp(db *gorm.DB) error {
+	if err := db.Where("id = ? AND user_id = ?", a.ID, a.UserID).First(&a).Error; err != nil {
+		return err
+	}
+	return nil
+}
