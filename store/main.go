96dff30d4c523624c1619718247a1fb3b056efe6
Adam Gall <adam@decentcrypto.com>
2018-11-09 14:21:42 -0500

    Pull database stuff out of store service

--- store/main.go
+++ store/main.go
@@ -13,12 +13,9 @@ import (
 
 	m "github.com/decentorganization/topaz/models"
 	shell "github.com/ipfs/go-ipfs-api"
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
 )
 
 var sh *shell.Shell
-var db *gorm.DB
 
 // Take the request body and store it in IPFS, then store the resulting hash in the 'objects' table.
 func requestHandler(w http.ResponseWriter, r *http.Request) {
@@ -46,31 +43,6 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// stmt := fmt.Sprintf("insert into objects (hash, user_id) values ('%s', '%s')",
-	// 	hash,
-	// 	os.Getenv("TOPAZ_USER"),
-	// )
-
-	o := m.Object{Hash: hash}
-	if err := db.Create(&o).Error; err != nil {
-		http.Error(
-			w,
-			fmt.Sprintf("error saving new object: %s", err.Error()),
-			http.StatusInternalServerError,
-		)
-		return
-	}
-
-	// _, err = db.Exec(stmt)
-	// if err != nil {
-	// 	http.Error(
-	// 		w,
-	// 		fmt.Sprintf("error adding ipfs hash to objects table: %s", err.Error()),
-	// 		http.StatusInternalServerError,
-	// 	)
-	// 	return
-	// }
-
 	log.Printf("processed incoming data with hash: %s", hash)
 
 	sr := m.StoreResponse{Hash: hash}
@@ -97,19 +69,6 @@ func main() {
 
 	shConn := fmt.Sprintf("%s:%s", os.Getenv("IPFS_HOST"), os.Getenv("IPFS_PORT"))
 	sh = shell.NewShell(shConn)
-
-	dbConn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable",
-		os.Getenv("PQ_HOST"),
-		os.Getenv("PQ_PORT"),
-		os.Getenv("PQ_USER"),
-		os.Getenv("PQ_NAME"),
-	)
-
-	db, err = gorm.Open("postgres", dbConn)
-	if err != nil {
-		log.Fatalf("couldn't even pretend to open database connection: %s", err.Error())
-	}
-	defer db.Close()
 
 	http.HandleFunc("/", requestHandler)
 
