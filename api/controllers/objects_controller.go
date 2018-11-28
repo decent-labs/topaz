49aac8395157f30126feeccfa94ffe857b05497f
Adam Gall <adam@decentcrypto.com>
2018-11-28 13:42:18 -0500

    Create endpoint and controller action for /report

--- api/controllers/objects_controller.go
+++ api/controllers/objects_controller.go
@@ -27,3 +27,13 @@ func Verify(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	w.WriteHeader(responseStatus)
 	w.Write(app)
 }
+
+func Report(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	appID, _ := strconv.Atoi(r.Header.Get("appId"))
+	body, _ := ioutil.ReadAll(r.Body)
+
+	responseStatus, app := services.Report(uint(appID), body)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(responseStatus)
+	w.Write(app)
+}
