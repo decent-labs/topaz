8d9385efd13bb5314c92092545997612f804cced
Adam Gall <adam@decentcrypto.com>
2019-01-29 13:34:41 -0500

    Update the objects_service for new objects / hashes scheme
    Accept hash instead of datablob in /trust request body

--- api/services/objects_service.go
+++ api/services/objects_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"net/http"
 	"time"
@@ -9,58 +10,60 @@ import (
 	"github.com/decentorganization/topaz/shared/models"
 )
 
-// Trust adds data to ipfs and creates a new 'object' in the database
-func Trust(appId uint, dataBlob []byte) (int, []byte) {
-	if len(dataBlob) == 0 {
-		return http.StatusBadRequest, []byte("no data")
+func Trust(appID uint, hash *models.Hash) (int, []byte) {
+	hb, err := hex.DecodeString(hash.HashHex)
+	if err != nil {
+		return http.StatusBadRequest, []byte("cannot decode hex hash")
 	}
+
+	if len(hb) != 32 {
+		return http.StatusBadRequest, []byte("invalid hash length")
+	}
+
+	t := time.Now().Unix()
 
 	o := models.Object{
-		DataBlob:      dataBlob,
-		AppID:         appId,
-		UnixTimestamp: time.Now().Unix(),
+		AppID:         appID,
+		UnixTimestamp: t,
 	}
-
-	hash, err := o.MakeHash()
-	if err != nil {
-		return http.StatusInternalServerError, []byte(err.Error())
-	}
-
-	o.Hash = hash
 
 	if err := o.CreateObject(database.Manager); err != nil {
 		return http.StatusInternalServerError, []byte(err.Error())
 	}
 
-	response, _ := json.Marshal(o)
-	return http.StatusOK, response
-}
-
-func Verify(appId uint, dataBlob []byte) (int, []byte) {
-	o := models.Object{
-		DataBlob: dataBlob,
+	h := models.Hash{
+		Hash:          hb,
+		UnixTimestamp: t,
+		ObjectID:      &o.ID,
 	}
 
-	hash, err := o.MakeHash()
-	if err != nil {
+	if err := h.CreateHash(database.Manager); err != nil {
 		return http.StatusInternalServerError, []byte(err.Error())
 	}
 
+	response, _ := json.Marshal(h)
+	return http.StatusOK, response
+}
+
+func Verify(appID uint, hash []byte) (int, []byte) {
 	so := models.Object{
-		AppID: appId,
-		Hash:  hash,
+		AppID: appID,
 	}
 
-	os := new(models.Objects)
-	if err := os.GetVerifiedObjects(database.Manager, &so); err != nil {
+	sh := models.Hash{
+		Hash: hash,
+	}
+
+	hs := new(models.Hashes)
+	if err := hs.GetVerifiedHashes(database.Manager, &so, &sh); err != nil {
 		return http.StatusInternalServerError, []byte(err.Error())
 	}
 
-	response, _ := json.Marshal(os)
+	response, _ := json.Marshal(hs)
 	return http.StatusOK, response
 }
 
-func Report(appId uint, body []byte) (int, []byte) {
+func Report(appID uint, body []byte) (int, []byte) {
 	var f interface{}
 	if err := json.Unmarshal(body, &f); err != nil {
 		return http.StatusInternalServerError, []byte(err.Error())
@@ -71,7 +74,7 @@ func Report(appId uint, body []byte) (int, []byte) {
 	end := int(m["end"].(float64))
 
 	os := new(models.Objects)
-	if err := os.GetObjectsByTimestamps(database.Manager, appId, start, end); err != nil {
+	if err := os.GetObjectsByTimestamps(database.Manager, appID, start, end); err != nil {
 		return http.StatusInternalServerError, []byte(err.Error())
 	}
 
