e183794ae9ca19fd568aebbf53a52ae5e0d7d22a
Adam Gall <adam@decentcrypto.com>
2019-02-05 14:30:34 -0500

    Add a PathPrefix to the router so that all routes are prepended with "/api/v1"

--- api/routers/v1/router.go
+++ api/routers/v1/router.go
@@ -7,6 +7,7 @@ import (
 // InitRoutes provisions our router with routes for various models
 func InitRoutes() *mux.Router {
 	r := mux.NewRouter()
+	r = r.PathPrefix("/api/v1/").Subrouter()
 	r = SetAuthenticationRoutes(r)
 	r = SetTestRoutes(r)
 	r = SetUsersRoutes(r)
