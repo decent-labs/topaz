5ca04a4206a1108c54df413a1857cbe2cdf8f914
Adam Gall <adam@decentcrypto.com>
2019-02-07 15:12:03 -0500

    Change database schema to use uuid instead of integer as primary keys for all tables

--- migrations/20181015142501_initial.sql
+++ migrations/20181015142501_initial.sql
@@ -1,7 +1,9 @@
 -- +migrate Up
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP,
@@ -11,52 +13,52 @@ CREATE TABLE users (
 );
 
 CREATE TABLE apps (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP,
     interval INTEGER NOT NULL,
     name CHARACTER varying(255) NOT NULL,
     last_batched INTEGER,
-    user_id INTEGER REFERENCES users(id),
+    user_id uuid REFERENCES users(id),
     eth_address CHARACTER varying(255) NOT NULL
 );
 
 CREATE TABLE batches (
-    id BIGSERIAL PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP,
-    app_id INTEGER REFERENCES apps(id),
+    app_id uuid REFERENCES apps(id),
     unix_timestamp INTEGER NOT NULL
 );
 
 CREATE TABLE proofs (
-    id BIGSERIAL PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP,
-    batch_id INTEGER REFERENCES batches(id),
+    batch_id uuid REFERENCES batches(id),
     merkle_root CHARACTER varying(255) NOT NULL,
     eth_transaction CHARACTER varying(255) NOT NULL
 );
 
 CREATE TABLE objects (
-    id BIGSERIAL PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP,
-    app_id INTEGER REFERENCES apps(id),
+    app_id uuid REFERENCES apps(id),
     uuid uuid NOT NULL
 );
 
 CREATE TABLE hashes (
-    id BIGSERIAL PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP,
-    object_id INTEGER REFERENCES objects(id),
-    proof_id INTEGER,
+    object_id uuid REFERENCES objects(id),
+    proof_id uuid,
     hash BYTEA NOT NULL,
     unix_timestamp INTEGER NOT NULL
 );
