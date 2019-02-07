9d192b3e82613ec55fbc223023f9978f1bbf9b3a
Adam Gall <adam@decentcrypto.com>
2019-02-07 17:24:45 -0500

    Add controller methods for handling getting all apps and getting single app for a user

--- api/controllers/apps_controller.go
+++ api/controllers/apps_controller.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/gorilla/mux"
+
 	"github.com/decentorganization/topaz/api/services"
 	"github.com/decentorganization/topaz/shared/models"
 )
@@ -18,6 +20,26 @@ func CreateApp(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 
 	rs, a := services.CreateApp(ra)
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(rs)
+	w.Write(a)
+}
+
+func GetApps(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	uid := r.Context().Value(models.UserID).(string)
+
+	rs, as := services.GetApps(uid)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(rs)
+	w.Write(as)
+}
+
+func GetApp(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	uid := r.Context().Value(models.UserID).(string)
+	aid := mux.Vars(r)["id"]
+
+	rs, a := services.GetApp(uid, aid)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(rs)
