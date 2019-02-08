bea15be41179c5cda376b19485cb5f26dcbe3a3a
Adam Gall <adam@decentcrypto.com>
2019-02-08 13:42:51 -0500

    Add object controller action for creating a new object

--- api/controllers/objects_controller.go
+++ api/controllers/objects_controller.go
@@ -1,8 +1,26 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/decentorganization/topaz/api/services"
+	"github.com/decentorganization/topaz/shared/models"
 )
+
+func CreateObject(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	uid := r.Context().Value(models.UserID).(string)
+
+	ro := new(models.Object)
+	d := json.NewDecoder(r.Body)
+	d.Decode(&ro)
+
+	h, o := services.CreateObject(ro, uid)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(h)
+	w.Write(o)
+}
 
 func Trust(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	// aid := r.Context().Value(models.AppID).(string)
