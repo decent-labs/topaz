64b52c81ed575e35d966cc702190c50342e16aac
Adam Gall <adam@decentcrypto.com>
2019-02-08 13:38:47 -0500

    Add a "create object" service function which accepts object stub including appID, makes sure that ID belongs to user, then creates new object

--- api/services/objects_service.go
+++ api/services/objects_service.go
@@ -10,6 +10,28 @@ import (
 	"github.com/decentorganization/topaz/shared/models"
 )
 
+func CreateObject(o *models.Object, uid string) (int, []byte) {
+	a := models.App{ID: o.AppID}
+	if err := a.FindApp(database.Manager); err != nil {
+		return http.StatusUnauthorized, []byte("")
+	}
+
+	if a.UserID != uid {
+		return http.StatusUnauthorized, []byte("")
+	}
+
+	if err := o.CreateObject(database.Manager); err != nil {
+		return http.StatusInternalServerError, []byte("")
+	}
+
+	r, err := json.Marshal(&o)
+	if err != nil {
+		return http.StatusInternalServerError, []byte("")
+	}
+
+	return http.StatusOK, r
+}
+
 func Trust(appID string, hash *models.Hash) (int, []byte) {
 	hb, err := hex.DecodeString(hash.HashHex)
 	if err != nil {
