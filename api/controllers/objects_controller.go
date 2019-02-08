4c83c2eb6a0a815f25b793ba0a6f9c14f6c3b5a3
Adam Gall <adam@decentcrypto.com>
2019-02-08 11:19:43 -0500

    Comment out all the object controller actions cause they're gonna be replaced in a future PR

--- api/controllers/objects_controller.go
+++ api/controllers/objects_controller.go
@@ -1,63 +1,57 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
-	"path"
-	"strconv"
-
-	"github.com/decentorganization/topaz/api/services"
-	"github.com/decentorganization/topaz/shared/models"
 )
 
 func Trust(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	aid := r.Context().Value(models.AppID).(string)
+	// aid := r.Context().Value(models.AppID).(string)
 
-	rh := new(models.Hash)
-	d := json.NewDecoder(r.Body)
-	d.Decode(&rh)
+	// rh := new(models.Hash)
+	// d := json.NewDecoder(r.Body)
+	// d.Decode(&rh)
 
-	rs, o := services.Trust(aid, rh)
+	// rs, o := services.Trust(aid, rh)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(rs)
-	w.Write(o)
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(rs)
+	// w.Write(o)
 }
 
 func TrustUpdate(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	aid := r.Context().Value(models.AppID).(string)
-	uuid := path.Base(r.URL.Path)
+	// aid := r.Context().Value(models.AppID).(string)
+	// uuid := path.Base(r.URL.Path)
 
-	rh := new(models.Hash)
-	d := json.NewDecoder(r.Body)
-	d.Decode(&rh)
+	// rh := new(models.Hash)
+	// d := json.NewDecoder(r.Body)
+	// d.Decode(&rh)
 
-	rs, h := services.TrustUpdate(aid, uuid, rh)
+	// rs, h := services.TrustUpdate(aid, uuid, rh)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(rs)
-	w.Write(h)
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(rs)
+	// w.Write(h)
 }
 
 func Verify(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	aid := r.Context().Value(models.AppID).(string)
-	o := path.Base(r.URL.Path)
+	// aid := r.Context().Value(models.AppID).(string)
+	// o := path.Base(r.URL.Path)
 
-	rs, os := services.Verify(aid, o)
+	// rs, os := services.Verify(aid, o)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(rs)
-	w.Write(os)
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(rs)
+	// w.Write(os)
 }
 
 func Report(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	aid := r.Context().Value(models.AppID).(string)
-	s, _ := strconv.Atoi(path.Base(path.Dir(r.URL.Path)))
-	e, _ := strconv.Atoi(path.Base(r.URL.Path))
+	// aid := r.Context().Value(models.AppID).(string)
+	// s, _ := strconv.Atoi(path.Base(path.Dir(r.URL.Path)))
+	// e, _ := strconv.Atoi(path.Base(r.URL.Path))
 
-	rs, os := services.Report(aid, s, e)
+	// rs, os := services.Report(aid, s, e)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(rs)
-	w.Write(os)
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(rs)
+	// w.Write(os)
 }
