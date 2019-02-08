22069074970144bf789f7fc06b5a5db3a237c343
Adam Gall <adam@decentcrypto.com>
2019-02-08 13:48:26 -0500

    Add object router handler for creating new object

--- api/routers/v1/objects.go
+++ api/routers/v1/objects.go
@@ -1,11 +1,21 @@
 package routers
 
 import (
+	"github.com/decentorganization/topaz/api/auth"
+	"github.com/decentorganization/topaz/api/controllers"
 	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 // SetObjectsRoutes provisions routes for 'object' activity
 func SetObjectsRoutes(r *mux.Router) *mux.Router {
+	s := r.PathPrefix("/objects").Subrouter()
+
+	// Create new object
+	s.Handle("", negroni.New(
+		negroni.HandlerFunc(auth.Auth),
+		negroni.HandlerFunc(controllers.CreateObject),
+	)).Methods("POST")
 
 	return r
 }
