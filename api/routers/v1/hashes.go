c335ae60767ac6eb99a807885814203ad07eb0a8
Adam Gall <adam@decentcrypto.com>
2019-02-11 15:07:42 -0500

    Create hashes router which mimics objects router

---
+++ api/routers/v1/hashes.go
@@ -0,0 +1,33 @@
+package routers
+
+import (
+	"github.com/decentorganization/topaz/api/authentication"
+	"github.com/decentorganization/topaz/api/controllers"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+// SetHashesRoutes provisions routes for 'hashes' activity
+func SetHashesRoutes(r *mux.Router) *mux.Router {
+	s := r.PathPrefix("/apps/{appId}/objects/{objectId}/hashes").Subrouter()
+
+	// Create new hash
+	s.Handle("", negroni.New(
+		negroni.HandlerFunc(authentication.Auth),
+		negroni.HandlerFunc(controllers.CreateHash),
+	)).Methods("POST")
+
+	// Get all hashes
+	s.Handle("", negroni.New(
+		negroni.HandlerFunc(authentication.Auth),
+		negroni.HandlerFunc(controllers.GetHashes),
+	)).Methods("GET")
+
+	// Get an hash
+	s.Handle("/{id}", negroni.New(
+		negroni.HandlerFunc(authentication.Auth),
+		negroni.HandlerFunc(controllers.GetHash),
+	)).Methods("GET")
+
+	return r
+}
