a9fe4e54c0da9429b71987e06e8519749362b3e2
Adam Gall <adam@decentcrypto.com>
2019-02-12 11:53:52 -0500

    Add proofs controller actions for getting proofs and proof

---
+++ api/controllers/proofs_controller.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/decentorganization/topaz/api/services"
+	"github.com/decentorganization/topaz/shared/models"
+	"github.com/gorilla/mux"
+)
+
+// GetProofs ...
+func GetProofs(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	h, p := services.GetProofs(
+		r.Context().Value(models.AuthUser).(*models.User),
+		mux.Vars(r)["appId"],
+		mux.Vars(r)["batchId"],
+	)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(h)
+	w.Write(p)
+}
+
+// GetProof ...
+func GetProof(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	h, p := services.GetProof(
+		r.Context().Value(models.AuthUser).(*models.User),
+		mux.Vars(r)["appId"],
+		mux.Vars(r)["batchId"],
+		mux.Vars(r)["id"],
+	)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(h)
+	w.Write(p)
+}
