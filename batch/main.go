e22b35280d0af5b9d518289614315aa1d3818f8e
Adam Gall <adam@decentcrypto.com>
2019-02-12 16:14:12 -0500

    Flatten out the main batch loop a bunch

--- batch/main.go
+++ batch/main.go
@@ -11,77 +11,48 @@ import (
 	"github.com/decentorganization/topaz/shared/models"
 )
 
-func getAppsToProof() (*models.Apps, error) {
-	apps := new(models.Apps)
-	err := apps.GetAppsToProof(database.Manager)
-	return apps, err
-}
-
-func getHashesToProof(app *models.App) (*models.Hashes, error) {
-	hashes := new(models.Hashes)
-	err := hashes.GetHashesByApp(database.Manager, app)
-	return hashes, err
-}
-
-func makeProof(hashes *models.Hashes, app *models.App, root string, tx string) (*models.Proof, error) {
-	ut := time.Now().Unix()
-
-	app.LastProofed = &ut
-
-	p := models.Proof{
-		App:            app,
-		MerkleRoot:     root,
-		EthTransaction: tx,
-		UnixTimestamp:  ut,
-	}
-
-	if err := p.CreateProof(database.Manager); err != nil {
-		return nil, err
-	}
-
-	if err := hashes.UpdateProof(database.Manager, &p.ID); err != nil {
-		return nil, err
-	}
-
-	return &p, nil
-}
-
-func newHashesFlow(a *models.App, hs *models.Hashes) {
-	root, err := hs.GetMerkleRoot()
-	if err != nil {
-		fmt.Errorf("couldn't create hash tree: " + err.Error())
-		return
-	}
-
-	tx, err := ethereum.Store(a.EthAddress, root)
-	if err != nil {
-		fmt.Errorf("couldn't store in contract: " + err.Error())
-		return
-	}
-
-	_, err = makeProof(hs, a, root, tx)
-	if err != nil {
-		fmt.Errorf("couldn't create proof: " + err.Error())
-		return
-	}
-}
-
 func mainLoop() {
-	apps, err := getAppsToProof()
-	if err != nil {
-		fmt.Errorf("didn't get apps to proof: " + err.Error())
+	apps := new(models.Apps)
+	if err := apps.GetAppsToProof(database.Manager); err != nil {
 		return
 	}
 
 	for _, a := range *apps {
-		hashes, err := getHashesToProof(&a)
-		if err != nil {
-			fmt.Errorf("couldn't get objects to proof: " + err.Error())
-			return
+		hashes := new(models.Hashes)
+		if err := hashes.GetHashesByApp(database.Manager, app); err != nil {
+			continue
 		}
 
-		if len(*hashes) > 0 {
-			newHashesFlow(&a, hashes)
+		if len(*hashes) = 0 {
+			continue
+		}
+	
+		root, err := hs.GetMerkleRoot()
+		if err != nil {
+			continue
+		}
+
+		tx, err := ethereum.Store(a.EthAddress, root)
+		if err != nil {
+			continue
+		}
+
+		ut := time.Now().Unix()
+		a.LastProofed = &ut
+		
+		p := models.Proof{
+			App:            a,
+			MerkleRoot:     root,
+			EthTransaction: tx,
+			UnixTimestamp:  ut,
+		}
+
+		if err := p.CreateProof(database.Manager); err != nil {
+			continue
+		}
+
+		if err := hashes.UpdateProof(database.Manager, &p.ID); err != nil {
+			continue
 		}
 	}
 }
