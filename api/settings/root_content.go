0deaaf1e6dba4b4a9b9eb71f535829dfb4c6f5f2
Adam Gall <adam@decentcrypto.com>
2019-02-20 11:42:44 -0500

    Create new file specifically for building out the root json

---
+++ api/settings/root_content.go
@@ -0,0 +1,30 @@
+package settings
+
+import "os"
+
+// Rc ...
+var Rc rootContent
+
+var version = "0.1.0"
+
+type rootContent struct {
+	Version         string          `json:"version"`
+	Environment     string          `json:"environment"`
+	EthereumContent ethereumContent `json:"ethereum"`
+}
+
+type ethereumContent struct {
+	EthereumNode     string `json:"node"`
+	EthereumContract string `json:"contract"`
+}
+
+func generateRootContent() {
+	Rc = rootContent{
+		Version:     version,
+		Environment: GetEnvironment(),
+		EthereumContent: ethereumContent{
+			EthereumNode:     os.Getenv("GETH_HOST"),
+			EthereumContract: os.Getenv("ETH_CONTRACT_ADDRESS"),
+		},
+	}
+}
