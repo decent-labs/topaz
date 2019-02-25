6cbe01380fa8afb20322552bd80724ff522b6dd8
Adam Gall <adam@decentcrypto.com>
2019-02-25 13:20:56 -0500

    Add route for accepting a new email address for the marketing list

---
+++ api/routers/routerV1/marketing.go
@@ -0,0 +1,18 @@
+package routerV1
+
+import (
+	"github.com/decentorganization/topaz/api/controllers"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+// SetMarketingRoutes ...
+func SetMarketingRoutes(r *mux.Router) *mux.Router {
+	s := r.PathPrefix("/marketing").Subrouter()
+
+	s.Handle("/emails", negroni.New(
+		negroni.HandlerFunc(controllers.CreateMarketingEmail),
+	)).Methods("POST")
+
+	return r
+}
