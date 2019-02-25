301717227560b578be5efe9f78cca5e2288b252f
Adam Gall <adam@decentcrypto.com>
2019-02-25 13:20:18 -0500

    Create marketings_controller for the new marketing email

---
+++ api/controllers/marketings_controller.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/decentorganization/topaz/api/services"
+	"github.com/decentorganization/topaz/shared/models"
+)
+
+// CreateMarketingEmail ...
+func CreateMarketingEmail(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	m := new(models.MarketingSiteEmail)
+	d := json.NewDecoder(r.Body)
+	d.Decode(&m)
+
+	rs := services.CreateMarketingEmail(m)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(rs)
+}
