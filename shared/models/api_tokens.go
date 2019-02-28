82baf71c0e79c47ec6ee8fa83f83b240b1d2d7c7
Adam Gall <adam@decentcrypto.com>
2019-02-27 21:14:40 -0500

    Create model for APITokens

---
+++ shared/models/api_tokens.go
@@ -0,0 +1,62 @@
+package models
+
+import (
+	"crypto/sha256"
+	"errors"
+	"math/rand"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	b58 "github.com/mr-tron/base58/base58"
+)
+
+// APIToken ...
+type APIToken struct {
+	ID        string     `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"-"`
+	UpdatedAt time.Time  `json:"-"`
+	DeletedAt *time.Time `sql:"index" json:"-"`
+
+	Token string `json:"token"`
+
+	UserID string `json:"userId"`
+	User   *User  `json:"-"`
+}
+
+// APITokens ...
+type APITokens []APIToken
+
+// CreateAPIToken ...
+func (a *APIToken) CreateAPIToken(db *gorm.DB) error {
+	if err := a.makeNewToken(); err != nil {
+		return err
+	}
+	return db.Create(&a).Error
+}
+
+// GetAPITokens ...
+func (as *APITokens) GetAPITokens(a *APIToken, db *gorm.DB) error {
+	return db.Model(&a.User).Related(&as).Error
+}
+
+// GetAPIToken ...
+func (a *APIToken) GetAPIToken(db *gorm.DB) error {
+	return db.Model(&a.User).Related(&a).Error
+}
+
+// GetAPITokenFromToken ...
+func (a *APIToken) GetAPITokenFromToken(db *gorm.DB) error {
+	if a.Token == "" {
+		return errors.New("no token")
+	}
+	return db.Where(&APIToken{Token: a.Token}).First(&a).Error
+}
+
+func (a *APIToken) makeNewToken() error {
+	rb := make([]byte, 256)
+	rand.Read(rb)
+	hash := sha256.Sum256(rb)
+	token := b58.Encode(hash[:])
+	a.Token = token
+	return nil
+}
