72abb25ee1869d4b9a0fc6e6549d2c96e19ce7d6
Adam Gall <adam@decentcrypto.com>
2019-05-02 13:19:50 -0400

    Add two new functions to blockchain model, one for getting blockchain network by ID, one for getting blockchain transactions by proof ID

--- shared/models/blockchain.go
+++ shared/models/blockchain.go
@@ -41,7 +41,22 @@ func (bt *BlockchainTransaction) CreateBlockchainTransaction(db *gorm.DB) error 
 	return db.Create(&bt).Error
 }
 
+// GetBlockchainNetwork ...
+func (bn *BlockchainNetwork) GetBlockchainNetwork(db *gorm.DB) error {
+	return db.Find(&bn).Error
+}
+
 // GetBlockchainNetworkFromName ...
 func (bn *BlockchainNetwork) GetBlockchainNetworkFromName(db *gorm.DB, name string) error {
 	return db.First(&bn, &BlockchainNetwork{Name: name}).Error
+}
+
+// GetBlockchainTransactionsByProof ..
+func (bts *BlockchainTransactions) GetBlockchainTransactionsByProof(db *gorm.DB, p *Proof) error {
+	return db.
+		Table("blockchain_transactions").
+		Where(&BlockchainTransaction{ProofID: p.ID}).
+		Order("created_at").
+		Find(&bts).
+		Error
 }
