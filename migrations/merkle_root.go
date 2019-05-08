2fd025b7e2ecc5f9fa76b6eb4fd33bc084099457
Adam Gall <adam@decentcrypto.com>
2019-05-08 10:02:21 -0400

    Better error handling

--- migrations/merkle_root.go
+++ migrations/merkle_root.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"strings"
 
 	"github.com/decentorganization/topaz/shared/database"
@@ -23,46 +22,56 @@ func (IntermediateProof) TableName() string {
 	return "proofs"
 }
 
-func merkleRootMigration(migrations migrate.MigrationSource) {
+func merkleRootMigration(migrations migrate.MigrationSource) (int, error) {
 	records, err := migrate.GetMigrationRecords(db, "postgres")
 	if err != nil {
-		log.Println("couldn't get migration records")
+		fmt.Println("couldn't get migration records")
+		return 0, err
 	}
 
 	lastRecord := records[len(records)-1]
 	if strings.Compare(lastRecord.Id, "20190502111700_blockchain_explorers.sql") == 0 {
-		n, err := migrate.ExecMax(db, "postgres", migrations, migrate.Up, 1)
+		n1, err := migrate.ExecMax(db, "postgres", migrations, migrate.Up, 1)
 		if err != nil {
-			log.Println("couldn't execute starting merkle root migration")
-			log.Fatal(err)
+			fmt.Println("couldn't execute starting merkle root migration")
+			return 0, err
 		}
 
-		fmt.Println("executed first", n, "merkle root migration")
-
-		convertRoots()
-
-		n, err = migrate.ExecMax(db, "postgres", migrations, migrate.Up, 1)
-		if err != nil {
-			log.Println("couldn't execute finishing merkle root migration")
-			log.Fatal(err)
+		if err := convertRoots(); err != nil {
+			return 0, err
 		}
 
-		fmt.Println("executed final", n, "merkle root migration")
+		n2, err := migrate.ExecMax(db, "postgres", migrations, migrate.Up, 1)
+		if err != nil {
+			fmt.Println("couldn't execute finishing merkle root migration")
+			return 0, err
+		}
+
+		return n1 + n2, nil
 	}
+
+	return 0, nil
 }
 
-func convertRoots() {
-	var intermediateProofs []IntermediateProof
-	database.Manager.Order("created_at").Find(&intermediateProofs)
+func convertRoots() error {
+	var ips []IntermediateProof
+	if err := database.Manager.Order("created_at").Find(&ips).Error; err != nil {
+		return err
+	}
 
-	for _, ip := range intermediateProofs {
+	for _, ip := range ips {
 		var mh multihash.Multihash
 
 		mh, err := multihash.FromB58String(ip.MerkleRoot)
 		if err != nil {
-			fmt.Println("oops decode didn't work")
+			return err
 		}
 
-		database.Manager.Model(&ip).UpdateColumn("merkle_root_bytes", mh)
+		if err := database.Manager.Model(&ip).UpdateColumn("merkle_root_bytes", mh).Error; err != nil {
+			return err
+		}
 	}
+
+	fmt.Println("performed", len(ips), "merkle root conversions")
+	return nil
 }
